87c5170661ef7bc0f7a23acf7eb010bde3e2e6bc
yms07 <yms07@YOSHIAKI-SURFAC>
2017-08-24 10:14:58 +0900

    追加1

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -39,6 +39,27 @@ print(b)
 print('c')
 a = 5+4
 print(a)
+print(a)
+b = 7
+print(b)
+print('c')
+a = 5+4
+print(a)
+b = 7
+print(b)
+print('c')
+print('c')
+print('c')
+a = 5+4
+print(a)
+b = 7
+print(b)
+print('c')
+a = 5+4
+print(a)
+b = 7
+print(b)
+print('c')
 b = 7
 print(b)
 print('c')
